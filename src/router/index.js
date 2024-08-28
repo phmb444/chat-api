b5d1386628f137d1a10dffd87d05c14cf1f9dcf8
Pedro Haubert <pedrohaubert7@gmail.com>
2024-08-28 16:24:02 -0300

    refactor: Update root route to serve API information

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Root route
-router.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
-});
-
 // Sobre route
-router.get('/sobre', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).send({
         'nome': 'API-CHAT',
         'versão':'0.1.0',
