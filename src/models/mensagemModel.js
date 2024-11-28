4c3bfa9bedcdaae1b245c1c184b40b7b8d6c7d7e
Pedro Haubert <pedrohaubert7@gmail.com>
2024-11-28 11:20:00 -0300

    oo

--- src/models/mensagemModel.js
+++ src/models/mensagemModel.js
@@ -12,13 +12,13 @@ async function enviarMensagem(data){
         return {error: "Token inválido"};
     }
     let decryptedToken = await token.decryptToken(data.headers.authorization);
-    let nick = decryptedToken.nick;
-    let idSala = new ObjectId(data.body.salaId);
+    let userNick = decryptedToken.nick;
+    let idSala = ObjectId.createFromTime(data.body.salaId);
     let mensagem = data.body.mensagem;
     let sala = await db.findOne('salas', {_id: idSala});
     console.log(sala);
     sala.mensagens.push({usuario: userNick, mensagem: mensagem, data : new Date()});
-    await db.updateOne('salas', {_id: salaId}, {$set: {mensagens: sala.mensagens}});
+    await db.updateOne('salas', {_id: idSala}, {$set: {mensagens: sala.mensagens}});
     return {mensagens: sala.mensagens, usuarios: sala.membros};
 }
 
