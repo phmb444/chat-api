b1b03edac0f43f2e2105366f8881a271abac15d8
Pedro Haubert <pedrohaubert7@gmail.com>
2024-11-28 11:23:06 -0300

    fix: Update mensagemModel to use ObjectId.createFromHexString for salaId and improve error handling

--- src/models/mensagemModel.js
+++ src/models/mensagemModel.js
@@ -3,7 +3,7 @@ const token = require('../util/token')
 const { ObjectId } = require('mongodb');
 
 async function listarMensagens(salaId) {
-    let sala = await db.findOne('salas', {_id: new ObjectId(salaId)});
+    let sala = await db.findOne('salas', {_id: ObjectId.createFromHexString(salaId)});
     return {mensagens: sala.mensagens, usuarios: sala.membros};
 }
 
@@ -13,13 +13,14 @@ async function enviarMensagem(data){
     }
     let decryptedToken = await token.decryptToken(data.headers.authorization);
     let userNick = decryptedToken.nick;
-    let idSala = ObjectId.createFromTime(data.body.salaId);
+    let idSala = ObjectId.createFromHexString(data.body.salaId);
     let mensagem = data.body.mensagem;
     let sala = await db.findOne('salas', {_id: idSala});
-    console.log(sala);
+    if (!sala) return { error: "Sala não encontrada" };
+    if (!sala.mensagens) sala.mensagens = [];
     sala.mensagens.push({usuario: userNick, mensagem: mensagem, data : new Date()});
     await db.updateOne('salas', {_id: idSala}, {$set: {mensagens: sala.mensagens}});
     return {mensagens: sala.mensagens, usuarios: sala.membros};
 }
 
-module.exports = {listarMensagens, enviarMensagem};
+module.exports = { listarMensagens, enviarMensagem };
